ff3e4bac76b4db8d30b64310bafe689e5ec1b272
Johan Garcia <john.jgp@gmail.com>
2020-01-02 11:40:22 -0300

    removed some comments

--- app/src/main/java/com/example/testcounter/data/transactions/Repository.kt
+++ app/src/main/java/com/example/testcounter/data/transactions/Repository.kt
@@ -12,15 +12,10 @@ class Repository @Inject constructor(val network: NetworkService) {
 
     fun fetchCounters() = network.fetchCounters().retry(2)
 
-    // TODO: Update counter with the serverId
     fun addCounter(title: String) =
         network.postCounter(counter = Counter(title =  title))
             .retry(2)
 
-    // TODO: Create a table to store unsynced changes, when persist on server fails, save the update in this table
-    // and call work manager to handle background sync, if no serverId is provided it means the counter has not been synced
-    // so the app needs to create it first, since the server only handles increase by 1 and decrease by 1, each update needs to be stored locally
-    // after all the data has been synced, update the counter that needs to be updated
     fun increaseCounter(counter: Counter) = network.increaseCounter(counter)
         .retry(2)
         .subscribeOn(Schedulers.io())
